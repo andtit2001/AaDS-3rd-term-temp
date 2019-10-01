82f6d55ac7bf092b3860c7069c1c741ebed5a21c
andtit2001 <andtit2001@gmail.com>
2019-10-01 16:25:11 +0300

    Add map-vector-based trie implementation

--- aho_corasick.hpp
+++ aho_corasick.hpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <map>
 #include <string>
 #include <vector>
 
@@ -44,6 +45,42 @@ class ITrie {
   template <class InputIter>
   NodeID Traverse(InputIter, InputIter, NodeID) const noexcept;
   virtual std::basic_string<CharT> GetString(NodeID) const;
+};
+
+template <class _CharT = char, class _SizeType = size_t>
+class MapVectorTrie : public ITrie<_SizeType, _CharT, _SizeType,
+                                   const std::map<_CharT, _SizeType>&> {
+ public:
+  using NodeID = typename MapVectorTrie::NodeID;
+  using CharT = typename MapVectorTrie::CharT;
+  using SizeType = typename MapVectorTrie::SizeType;
+  using IterableWrapper = typename MapVectorTrie::IterableWrapper;
+
+ protected:
+  struct Node : ITrie<SizeType, CharT, SizeType, IterableWrapper>::ITrieNode {
+    std::map<CharT, SizeType> transitions;
+  };
+
+  std::vector<Node> nodes_;
+  std::vector<NodeID> terminal_nodes_;
+
+  virtual NodeID AddNode(NodeID, CharT) override;
+  virtual void AddEntryToNode(NodeID, SizeType) override;
+
+ public:
+  MapVectorTrie();
+
+  // Dictionary interface
+  virtual const std::vector<SizeType>& GetDictionaryEntries(NodeID) const
+      noexcept override;
+  virtual std::basic_string<CharT> GetStringByIndex(SizeType) const override;
+
+  // Tree interface
+  virtual NodeID Root() const noexcept override;
+  virtual NodeID GetNext(NodeID, CharT) const noexcept override;
+  virtual NodeID GetParent(NodeID) const noexcept override;
+  virtual CharT GetIncomingChar(NodeID) const noexcept override;
+  virtual IterableWrapper GetOutcomingEdges(NodeID) const override;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -113,4 +150,77 @@ ITrie<_NodeID, _CharT, _SizeType, _IterableWrapper>::GetString(
     string.push_back(GetIncomingChar(node));
   std::reverse(string.begin(), string.end());
   return string;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <class _CharT, class _SizeType>
+MapVectorTrie<_CharT, _SizeType>::MapVectorTrie() : nodes_(1) {
+  nodes_.front().parent = 0;
+  nodes_.front().prev_char = '\0';
+}
+
+template <class _CharT, class _SizeType>
+typename MapVectorTrie<_CharT, _SizeType>::NodeID
+MapVectorTrie<_CharT, _SizeType>::AddNode(NodeID node, CharT ch) {
+  nodes_[node].transitions[ch] = nodes_.size();
+  nodes_.emplace_back();
+  nodes_.back().parent = node;
+  nodes_.back().prev_char = ch;
+  return nodes_.size() - 1;
+}
+
+template <class _CharT, class _SizeType>
+void MapVectorTrie<_CharT, _SizeType>::AddEntryToNode(NodeID node,
+                                                      SizeType index) {
+  nodes_[node].entries.emplace_back(index);
+  if (index >= terminal_nodes_.size())
+    terminal_nodes_.resize(index + 1);
+  terminal_nodes_[index] = node;
+}
+
+template <class _CharT, class _SizeType>
+const std::vector<typename MapVectorTrie<_CharT, _SizeType>::SizeType>&
+MapVectorTrie<_CharT, _SizeType>::GetDictionaryEntries(NodeID node) const
+    noexcept {
+  return nodes_[node].entries;
+}
+
+template <class _CharT, class _SizeType>
+std::basic_string<typename MapVectorTrie<_CharT, _SizeType>::CharT>
+MapVectorTrie<_CharT, _SizeType>::GetStringByIndex(SizeType index) const {
+  return this->GetString(terminal_nodes_[index]);
+}
+
+template <class _CharT, class _SizeType>
+typename MapVectorTrie<_CharT, _SizeType>::NodeID
+MapVectorTrie<_CharT, _SizeType>::Root() const noexcept {
+  return 0;
+}
+
+template <class _CharT, class _SizeType>
+typename MapVectorTrie<_CharT, _SizeType>::NodeID
+MapVectorTrie<_CharT, _SizeType>::GetNext(NodeID node, CharT ch) const
+    noexcept {
+  auto it = nodes_[node].transitions.find(ch);
+  if (it == nodes_[node].transitions.end()) return node;
+  return it->second;
+}
+
+template <class _CharT, class _SizeType>
+typename MapVectorTrie<_CharT, _SizeType>::NodeID
+MapVectorTrie<_CharT, _SizeType>::GetParent(NodeID node) const noexcept {
+  return nodes_[node].parent;
+}
+
+template <class _CharT, class _SizeType>
+typename MapVectorTrie<_CharT, _SizeType>::CharT
+MapVectorTrie<_CharT, _SizeType>::GetIncomingChar(NodeID node) const noexcept {
+  return nodes_[node].prev_char;
+}
+
+template <class _CharT, class _SizeType>
+typename MapVectorTrie<_CharT, _SizeType>::IterableWrapper
+MapVectorTrie<_CharT, _SizeType>::GetOutcomingEdges(NodeID node) const {
+  return nodes_[node].transitions;
 }
